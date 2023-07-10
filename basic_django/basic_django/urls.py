d76adcaa4feafc73c3f43b4b84bb9afd4c07acdf
Rabindra <rabindrasapkota@lftechnology.com>
2023-07-11 01:00:24 +0545

    Traffic routed till view

--- basic_django/basic_django/urls.py
+++ basic_django/basic_django/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from favmusics import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('customer_info/<int:customer_id>/', views.customer_info, name='customer_info'),
 ]
--- basic_django/favmusics/views.py
+++ basic_django/favmusics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<p>Reached Till View</p>')
+
+
+def customer_info(request, customer_id):
+    return HttpResponse(f'<p>Trying to search for: {customer_id}</p>')
