98daf2af29451ff91cd1f179f240172788bd9512
Rabindra <rabindrasapkota@lftechnology.com>
2023-07-11 00:50:26 +0545

    Model added with Admin Configuration

--- basic_django/favmusics/admin.py
+++ basic_django/favmusics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer, Music
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dob', 'gender']
+
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    pass
