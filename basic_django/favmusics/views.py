378ac82f98f02755421e04a3c07ee5f6eb38ff6d
Rabindra <rabindrasapkota@lftechnology.com>
2023-07-11 01:12:03 +0545

    Traffic Routed till template

--- basic_django/favmusics/views.py
+++ basic_django/favmusics/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Customer
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>Reached Till View</p>')
+    customers = Customer.objects.all()
+    return render(request, 'home.html', {"customers": customers})
 
 
 def customer_info(request, customer_id):
-    return HttpResponse(f'<p>Trying to search for: {customer_id}</p>')
+    try:
+        customer = Customer.objects.get(id=customer_id)
+    except Customer.DoesNotExist:
+        raise Http404('Customer not found')
+    return render(request, 'customer_detail.html', {'customer': customer})
